1dde07751383ebc0c1bddb0b7caa17d536101d1a
JoschD <26184899+JoschD@users.noreply.github.com>
2021-12-10 11:48:24 +0100

    changed config as in mkdocs material example

--- docs/javascripts/mathjax-config.js
+++ docs/javascripts/mathjax-config.js
@@ -1,11 +1,27 @@
-/* mathjax-loader.js  file */
-/* http://facelessuser.github.io/pymdown-extensions/extensions/arithmatex/ */
-/* https://docs.mathjax.org/en/latest/input/tex/eqnumbers.html */
-/* https://docs.mathjax.org/en/latest/options/index.html */
+/* mathjax-config.js  file
+refs: 
+https://squidfunk.github.io/mkdocs-material/setup/extensions/python-markdown-extensions/#arithmatex
+https://docs.mathjax.org/en/latest/input/tex/eqnumbers.html
+ https://docs.mathjax.org/en/latest/options/index.html
+*/
+
 window.MathJax = {
   tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
     tags: 'ams',
     tagSide: 'right',
     tagIndent: '1.2em',
   },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  }
 };
+
+/* Given in markdown example, but creates weird boxes */
+// document$.subscribe(() => {
+//   MathJax.typesetPromise()
+// })
