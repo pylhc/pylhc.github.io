487feb72793a2d777c59c92021334adce5c88f19
JoschD <26184899+JoschD@users.noreply.github.com>
2023-10-30 17:26:43 +0100

    added holiday night shifts

--- scripts/shift_calculations.py
+++ scripts/shift_calculations.py
@@ -77,8 +77,16 @@ def str_to_dt(date_str: str) -> datetime:
 
 # Check dates ------------------------------------------------------------------
 WORK = "W"
-NIGHT = "N"  # on a weekday
+WORK_NIGHT = "WN" 
 HOLIDAY = "H"
+HOLIDAY_NIGHT = "HN"
+ALL_SHIFTS = (WORK, WORK_NIGHT, HOLIDAY, HOLIDAY_NIGHT)
+SHIFT_NAMING = {
+    WORK: "Mo - Fr (Day)",
+    WORK_NIGHT: "Mo - Fr (Night)",
+    HOLIDAY: "Holiday/Weekend (Day)",
+    HOLIDAY_NIGHT: "Holiday/Weekend (Night)",
+}
 
 WORK_START_TIME = {"hour": 8, "minute": 30, "second": 0}
 WORK_END_TIME  = {"hour": 17, "minute": 30, "second": 0}
@@ -123,7 +131,7 @@ def is_holiday(date: datetime):
 
 
 def calculate_shift_parts(start_time: datetime, end_time: datetime) -> Dict[str, timedelta]:
-    """Split the given shift into work hours, outside working hours and holidays/weekends.
+    """Split the given shift into work hours, holidays/weekends day or night shifts.
 
     Args:
         start_time (datetime): Start time of the shift 
@@ -139,40 +147,34 @@ def calculate_shift_parts(start_time: datetime, end_time: datetime) -> Dict[str,
     if start_time > end_time:
         raise ValueError(f"Start time {start_time} is after end time {end_time}")
     
-    time_split = {
-        WORK: timedelta(),
-        NIGHT: timedelta(),
-        HOLIDAY: timedelta(),
-    }
+    time_split = {shift: timedelta() for shift in ALL_SHIFTS}
 
     current_time = start_time
     while current_time < end_time:
-        day_end = current_time.replace(day=current_time.day + 1, hour=0, minute=0, second=0)
-
         if current_time.weekday() < 5 and not is_holiday(current_time):  # Weekday (Monday to Friday)
-            # working hours on this day
-            work_start = current_time.replace(**WORK_START_TIME)
-            work_end = current_time.replace(**WORK_END_TIME)
+            day_shift, night_shift = WORK, WORK_NIGHT
+        else:
+            day_shift, night_shift = HOLIDAY, HOLIDAY_NIGHT
+        
+        # hours on this day
+        day_end = current_time.replace(day=current_time.day + 1, hour=0, minute=0, second=0)
+        work_start = current_time.replace(**WORK_START_TIME)
+        work_end = current_time.replace(**WORK_END_TIME)
 
-            if current_time < work_start:
-                # difference to work starting time (or shift)
-                time_split[NIGHT] += min(work_start, end_time) - current_time
-                current_time = work_start
+        if current_time < work_start:
+            # difference to work starting time (or shift)
+            time_split[night_shift] += min(work_start, end_time) - current_time
+            current_time = work_start
 
-            elif current_time >= work_end:
-                # difference to end of the day (or shift)
-                time_split[NIGHT] += min(day_end, end_time) - current_time
-                current_time = day_end
-
-            else:
-                # difference to work ending time (or shift ending)
-                time_split[WORK] += min(work_end, end_time) - current_time
-                current_time = work_end
-
-        else:  # Weekend (Saturday or Sunday) or holiday
+        elif current_time >= work_end:
             # difference to end of the day (or shift)
-            time_split[HOLIDAY] += min(day_end, end_time) - current_time
+            time_split[night_shift] += min(day_end, end_time) - current_time
             current_time = day_end
+
+        else:
+            # difference to work ending time (or shift ending)
+            time_split[day_shift] += min(work_end, end_time) - current_time
+            current_time = work_end
 
     return time_split 
 
@@ -203,8 +205,9 @@ def test_working_hours_friday():
         end_time=datetime(2023, 10, 28, 4, 0)
     )
     assert abs(time_delta_to_hours(parts[WORK]) - 1.5) < EPS
-    assert abs(time_delta_to_hours(parts[NIGHT]) - 6.5) < EPS
-    assert abs(time_delta_to_hours(parts[HOLIDAY]) - 4.0) < EPS
+    assert abs(time_delta_to_hours(parts[WORK_NIGHT]) - 6.5) < EPS
+    assert abs(time_delta_to_hours(parts[HOLIDAY]) - 0.0) < EPS
+    assert abs(time_delta_to_hours(parts[HOLIDAY_NIGHT]) - 4.0) < EPS
 
 
 def test_working_hours_monday_wednesday():
@@ -213,8 +216,9 @@ def test_working_hours_monday_wednesday():
         end_time=datetime(2023, 10, 25, 16, 0)
     )
     assert abs(time_delta_to_hours(parts[WORK]) - 25.5) < EPS
-    assert abs(time_delta_to_hours(parts[NIGHT]) - 31.5) < EPS
+    assert abs(time_delta_to_hours(parts[WORK_NIGHT]) - 31.5) < EPS
     assert abs(time_delta_to_hours(parts[HOLIDAY]) - 0) < EPS
+    assert abs(time_delta_to_hours(parts[HOLIDAY_NIGHT]) - 0) < EPS
 
 
 def test_working_hours_single_day():
@@ -223,8 +227,9 @@ def test_working_hours_single_day():
         end_time=datetime(2023, 10, 23, 16, 0)
     )
     assert abs(time_delta_to_hours(parts[WORK]) - 7) < EPS
-    assert abs(time_delta_to_hours(parts[NIGHT]) - 0) < EPS
+    assert abs(time_delta_to_hours(parts[WORK_NIGHT]) - 0) < EPS
     assert abs(time_delta_to_hours(parts[HOLIDAY]) - 0) < EPS
+    assert abs(time_delta_to_hours(parts[HOLIDAY_NIGHT]) - 0) < EPS
 
 
 # Main --------------------------------------------------------------------------
@@ -241,11 +246,9 @@ def calculate_shifts(file_path: Union[str, Path]):
     """
     file_path = Path(file_path)
     df = parse_file(file_path)
-    parts = {
-        WORK: timedelta(),
-        NIGHT: timedelta(),
-        HOLIDAY: timedelta(),
-    }
+
+    parts = {shift: timedelta() for shift in ALL_SHIFTS}
+
     if not all(c in df.columns for c in [COLUMN_START, COLUMN_END]):
         raise ValueError(f"No start or end time column found in {file_path.name}")
 
@@ -261,12 +264,8 @@ def calculate_shifts(file_path: Union[str, Path]):
             parts[key] += value
 
     print(f"\nShifts from '{COLUMN_START}'/'{COLUMN_END}' columns in File {file_path.name}") 
-    # print(f"Time spend during working hours: {time_delta_to_hours(parts[WORK])}h")
-    # print(f"Time spend outside of working hours: {time_delta_to_hours(parts[OUTSIDE])}h")
-    # print(f"Time spend on holidays or weekends: {time_delta_to_hours(parts[HOLIDAY])}h")
-    print(f"Shifts during working hours: {time_delta_to_shifts(parts[WORK])}")
-    print(f"Shifts outside of working hours: {time_delta_to_shifts(parts[NIGHT])}")
-    print(f"Shifts on holidays or weekends: {time_delta_to_shifts(parts[HOLIDAY])}")
+    for shift, name in SHIFT_NAMING.items():
+        print(f"{name}: {parts[shift]}")
 
     return parts
 
@@ -285,11 +284,7 @@ def manual_shifts(file_path: Union[str, Path]):
     file_path = Path(file_path)
     df = parse_file(file_path)
 
-    parts = {
-        WORK: 0.0,
-        NIGHT: 0.0, 
-        HOLIDAY: 0.0,
-    }
+    parts = {shift: 0.0 for shift in ALL_SHIFTS}
 
     all_names = "".join(parts.keys())
 
@@ -305,12 +300,8 @@ def manual_shifts(file_path: Union[str, Path]):
             parts[key] += float(value)
 
     print(f"\nShifts from '{COLUMN_SHIFTS}' column in File {file_path.name}") 
-    # print(f"Time spend during working hours: {time_delta_to_hours(parts[WORK])}h")
-    # print(f"Time spend outside of working hours: {time_delta_to_hours(parts[OUTSIDE])}h")
-    # print(f"Time spend on holidays or weekends: {time_delta_to_hours(parts[HOLIDAY])}h")
-    print(f"Shifts during working hours: {parts[WORK]}")
-    print(f"Shifts outside of working hours: {parts[NIGHT]}")
-    print(f"Shifts on holidays or weekends: {parts[HOLIDAY]}")
+    for shift, name in SHIFT_NAMING.items():
+        print(f"{name}: {parts[shift]}")
 
     return parts
 
@@ -324,14 +315,9 @@ def plot_results(parts, title: str = None, output_path: Union[str, Path] = None)
         output_path (Union[str, Path]): Path to the output file.
     """
     fig, ax = plt.subplots()
-    name_map = {
-        WORK: "Working hour shifts",
-        NIGHT: "Night shifts",
-        HOLIDAY: "Holidays or weekends",
-    }
 
     data = [time_delta_to_shifts(value) if isinstance(value, timedelta) else value for value in parts.values() ]
-    labels = [f"{name_map[k]}: {v:.1f}" for k, v in zip(parts.keys(), data)]
+    labels = [f"{SHIFT_NAMING[k]}: {v:.1f}" for k, v in zip(parts.keys(), data)]
 
     ax.pie(data, labels=labels, autopct='%1.1f%%')
 
